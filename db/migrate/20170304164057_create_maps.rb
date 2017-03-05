5ebbb1879c50a8af752465deaca4ef4c9fa62f2e
Sarah Vessels <cheshire137@gmail.com>
2017-03-04 19:24:04 -0600

    Rename maps.type to maps.map_type
    To avoid Rails thinking it's for single-table inheritance.

--- db/migrate/20170304164057_create_maps.rb
+++ db/migrate/20170304164057_create_maps.rb
@@ -2,7 +2,7 @@ class CreateMaps < ActiveRecord::Migration[5.0]
   def change
     create_table :maps do |t|
       t.string :name, null: false
-      t.string :type
+      t.string :map_type
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20170304172632) do
 
   create_table "maps", force: :cascade do |t|
     t.string   "name",       null: false
-    t.string   "type"
+    t.string   "map_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
