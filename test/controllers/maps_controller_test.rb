51c3ddcff267d55cbec2aaed8af34a440a1fcb24
Sarah Vessels <cheshire137@gmail.com>
2017-03-05 09:56:14 -0600

    Add checks to test about map data in response

--- test/controllers/maps_controller_test.rb
+++ test/controllers/maps_controller_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class MapsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @map = create(:map)
+    @map_segment = create(:map_segment, map: @map)
+  end
+
   test "loads index JSON" do
     get "/api/maps"
     assert_response :success
+    assert_includes response.body, @map.name
+    assert_includes response.body, @map.map_type
+    assert_includes response.body, @map_segment.name
   end
 end
