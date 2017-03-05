63c2be1fd7a3ad5659500cb3907a7a6eacedff40
Sarah Vessels <cheshire137@gmail.com>
2017-03-05 10:28:41 -0600

    Add PlayerHero validations and tests

--- test/models/player_hero_test.rb
+++ test/models/player_hero_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class PlayerHeroTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "requires a player" do
+    player_hero = PlayerHero.new
+    refute_predicate player_hero, :valid?
+    assert_predicate player_hero.errors[:player], :any?
+  end
+
+  test "requires a hero" do
+    player_hero = PlayerHero.new
+    refute_predicate player_hero, :valid?
+    assert_predicate player_hero.errors[:hero], :any?
+  end
+
+  test "requires an integer confidence" do
+    player_hero = PlayerHero.new(confidence: 15.5)
+    refute_predicate player_hero, :valid?
+    assert_predicate player_hero.errors[:confidence], :any?
+  end
 end
