1d163d4f547fd8481ab7e63eca3da8c000bef4b2
Sarah Vessels <cheshire137@gmail.com>
2017-03-05 10:26:01 -0600

    Validate uniqueness of map + name combo

--- app/models/map_segment.rb
+++ app/models/map_segment.rb
@@ -2,4 +2,5 @@ class MapSegment < ApplicationRecord
   belongs_to :map
 
   validates :map, :name, presence: true
+  validates :map_id, uniqueness: { scope: :name }
 end
--- test/models/map_segment_test.rb
+++ test/models/map_segment_test.rb
@@ -12,4 +12,14 @@ class MapSegmentTest < ActiveSupport::TestCase
     refute_predicate map_segment, :valid?
     assert_predicate map_segment.errors[:map], :any?
   end
+
+  test "requires a unique map + name combination" do
+    map = Map.create!(name: "Route 66")
+    map_segment1 = MapSegment.create!(name: "Payload 1", map: map)
+
+    map_segment2 = MapSegment.new(name: map_segment1.name,
+                                  map: map_segment1.map)
+    refute_predicate map_segment2, :valid?
+    assert_predicate map_segment2.errors[:map_id], :any?
+  end
 end
