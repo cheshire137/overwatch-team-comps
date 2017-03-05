b58c6f5a7c4e3f029f4131ee51b2c66d3904e86d
Sarah Vessels <cheshire137@gmail.com>
2017-03-05 11:09:58 -0600

    Drop jQuery, CoffeeScript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require react
 //= require react_ujs
 //= require components
