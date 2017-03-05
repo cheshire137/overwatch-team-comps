1a17a1dc662ce7ded94f46e5536c3406bfd1b58a
Sarah Vessels <cheshire137@gmail.com>
2017-03-05 11:34:14 -0600

    Hit /api/maps endpoint to list maps in page

--- app/assets/javascripts/components/app.es6.jsx
+++ app/assets/javascripts/components/app.es6.jsx
@@ -1,7 +1,34 @@
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = { maps: [] }
+  }
+
+  componentDidMount() {
+    const api = new OverwatchTeamCompsApi()
+    api.getMaps().then(maps => this.onMapsFetched(maps)).
+      catch(err => this.onMapsError(err))
+  }
+
+  onMapsFetched(maps) {
+    this.setState({ maps })
+  }
+
+  onMapsError(error) {
+    console.error('failed to load maps', error)
+  }
+
   render () {
+    const { maps } = this.state
+    if (maps.length < 1) {
+      return <p>hello world</p>
+    }
     return (
-      <p>hello world</p>
+      <ul>{
+        maps.map(map => {
+          return <li key={map.name}>{map.name}</li>
+        })
+      }</ul>
     )
   }
 }
