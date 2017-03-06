425d379901ef21083f8563b7a9a604bb3d0b5186
Sarah Vessels <cheshire137@gmail.com>
2017-03-06 11:43:41 -0600

    Add OmniAuth callbacks controller for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   scope defaults: { format: :json }, path: "/api" do
     resources :maps, only: [:index]
