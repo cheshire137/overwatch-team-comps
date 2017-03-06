b90353a4dd65205e2565872baa2007a48f98496f
Sarah Vessels <cheshire137@gmail.com>
2017-03-06 16:35:00 -0600

    Add some relations to Composition for players

--- app/models/composition.rb
+++ app/models/composition.rb
@@ -2,5 +2,10 @@ class Composition < ApplicationRecord
   belongs_to :map
   belongs_to :user
 
+  has_many :player_selections
+  has_many :player_heroes, through: :player_selections
+  has_many :players, through: :player_heroes
+  has_many :heroes, through: :player_heroes
+
   validates :map, presence: true
 end
