51513998e5eb9596df9bc770878bf1969a5ca925
Sarah Vessels <cheshire137@gmail.com>
2017-03-06 11:23:23 -0600

    Add bnet omniauth config to Devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -248,6 +248,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :bnet, ENV['BNET_APP_ID'], ENV['BNET_APP_SECRET'],
+    callback_url: '/users/auth/bnet/callback'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
