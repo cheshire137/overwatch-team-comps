ba1a967c4d87e7507d63c969fcb886132e181afe
Sarah Vessels <cheshire137@gmail.com>
2017-03-06 19:40:17 -0600

    Get separate players list

--- app/controllers/compositions_controller.rb
+++ app/controllers/compositions_controller.rb
@@ -4,7 +4,13 @@ class CompositionsController < ApplicationController
     @players = []
 
     Composition::MAX_PLAYERS.times do |i|
-      @players << Player.new(name: "Player #{i + 1}", user: current_user)
+      player = Player.new(name: "Player #{i + 1}", user: current_user)
+      @players << player
+
+      player_hero = PlayerHero.new(player: player)
+      @composition.player_selections << PlayerSelection.new(
+        player_hero: player_hero, composition: @composition
+      )
     end
   end
 end
