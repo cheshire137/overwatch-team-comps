1b3d605e0edccf1116df3f45340ee4f900c656c7
Sarah Vessels <cheshire137@gmail.com>
2017-03-06 20:50:14 -0600

    Add route alias for /pages/:page

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see
   # http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
-end
 
-Rails.application.routes.draw do
-  get "/pages/:page" => "pages#show"
+  get "/pages/:page" => "pages#show", as: :page
 end
