df8f344ca688d7d3e285eee85e6e016a03542dc1
Sarah Vessels <cheshire137@gmail.com>
2017-03-06 19:55:28 -0600

    Add Hero factory for tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,16 @@
 FactoryGirl.define do
+  factory :hero do
+    name 'McCree'
+    role 'offense'
+  end
+
   factory :map do
-    name "Dorado"
-    map_type "escort"
+    name 'Dorado'
+    map_type 'escort'
   end
 
   factory :map_segment do
     map
-    name "Attacking: Payload 1"
+    name 'Attacking: Payload 1'
   end
 end
