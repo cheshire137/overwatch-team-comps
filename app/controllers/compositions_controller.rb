0a5c0f5a82d0458624d71a576d86a07c0c7b836e
Sarah Vessels <cheshire137@gmail.com>
2017-03-06 19:33:59 -0600

    Move new comp building into model

--- app/controllers/compositions_controller.rb
+++ app/controllers/compositions_controller.rb
@@ -1,12 +1,5 @@
 class CompositionsController < ApplicationController
   def new
-    @composition = Composition.new
-    @composition.map = Map.first
-    6.times do |i|
-      player = Player.new(name: "Player #{i + 1}")
-      player_hero = PlayerHero.new(player: player)
-      @composition.player_selections << PlayerSelection.new(player_hero: player_hero,
-                                                            composition: @composition)
-    end
+    @composition = Composition.build_new(user: current_user, map: Map.first)
   end
 end
--- app/models/composition.rb
+++ app/models/composition.rb
@@ -1,4 +1,6 @@
 class Composition < ApplicationRecord
+  MAX_PLAYERS = 6
+
   belongs_to :map
   belongs_to :user
 
@@ -8,4 +10,18 @@ class Composition < ApplicationRecord
   has_many :heroes, through: :player_heroes
 
   validates :map, presence: true
+
+  def self.build_new(user:, map:)
+    comp = Composition.new(map: map)
+
+    MAX_PLAYERS.times do |i|
+      player = Player.new(name: "Player #{i + 1}", user: user)
+      player_hero = PlayerHero.new(player: player)
+      comp.player_selections << PlayerSelection.new(
+        player_hero: player_hero, composition: comp
+      )
+    end
+
+    comp
+  end
 end
