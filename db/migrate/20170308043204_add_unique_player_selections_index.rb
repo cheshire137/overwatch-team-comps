d6f248a1c34d6d80d558546b3daec470308eb109
Sarah Vessels <cheshire137@gmail.com>
2017-03-07 22:54:41 -0600

    Don't need extra migration now

--- db/migrate/20170308043204_add_unique_player_selections_index.rb
+++
@@ -1,6 +0,0 @@
-class AddUniquePlayerSelectionsIndex < ActiveRecord::Migration[5.0]
-  def change
-    add_index :player_selections, [:map_segment_id, :composition_id, :player_hero_id],
-      unique: true, name: 'idx_player_selections_unique_combo'
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308043204) do
+ActiveRecord::Schema.define(version: 20170308042755) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
