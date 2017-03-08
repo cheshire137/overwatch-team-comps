e95ed439e22ae45c0ca04dec3558965fe4bfecaa
Rick Winfrey <rewinfrey@github.com>
2017-03-07 23:01:10 -0800

    Update compositions table
    - Adds session_id column
    - Updates user_id to be not null

--- db/migrate/20170307224336_add_video_url_to_compositions.rb
+++ db/migrate/20170307224336_add_video_url_to_compositions.rb
@@ -1,5 +1,7 @@
 class AddVideoUrlToCompositions < ActiveRecord::Migration[5.0]
   def change
     add_column :compositions, :video_url, :string, null: true, default: ''
+    add_column :compositions, :session_id, :string, null: false, limit: 32
+    change_column :compositions, :user_id, :integer, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,11 +18,12 @@ ActiveRecord::Schema.define(version: 20170307224336) do
   create_table "compositions", force: :cascade do |t|
     t.string   "name"
     t.text     "notes"
-    t.integer  "map_id",                  null: false
-    t.integer  "user_id"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.string   "video_url",  default: ""
+    t.integer  "map_id",                             null: false
+    t.integer  "user_id",                            null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.string   "video_url",             default: ""
+    t.string   "session_id", limit: 32,              null: false
     t.index ["map_id"], name: "index_compositions_on_map_id", using: :btree
     t.index ["user_id"], name: "index_compositions_on_user_id", using: :btree
   end
