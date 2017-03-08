c5daea77644d810b80c930b2bcb6e800a8b7791e
Sarah Vessels <cheshire137@gmail.com>
2017-03-07 20:27:29 -0600

    Pull map segment header from response

---
+++ app/assets/javascripts/components/map-segment-header.jsx
@@ -0,0 +1,20 @@
+class MapSegmentHeader extends React.Component {
+  className() {
+    if (this.props.mapSegment.toLowerCase().indexOf('attacking') > -1) {
+      return 'attack-cell'
+    }
+    return 'defend-cell'
+  }
+
+  render() {
+    return (
+      <th className={this.className()}>{this.props.mapSegment}</th>
+    )
+  }
+}
+
+MapSegmentHeader.propTypes = {
+  mapSegment: React.PropTypes.string.isRequired
+}
+
+export default MapSegmentHeader
