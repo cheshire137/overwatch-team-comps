d31e2b00594c05e87b73defdeddb9419ab288649
Rick Winfrey <rewinfrey@github.com>
2017-03-07 22:31:43 -0800

    Add anonymous user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -72,3 +72,9 @@ map_segments_by_map.each do |map_name, base_segments|
     MapSegment.create(map_id: map.id, name: segment)
   end
 end
+
+puts "Creating anonymous user"
+User.create(
+  email: "anonymous@ghost.com",
+  password: "passworD1"
+)
