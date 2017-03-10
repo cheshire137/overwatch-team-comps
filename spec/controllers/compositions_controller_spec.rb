eaa548559fb593075b56c311d81ba782788f728d
Sarah Vessels <cheshire137@gmail.com>
2017-03-10 17:44:34 -0600

    Response tests as request specs, side effect tests as controller specs

--- spec/controllers/compositions_controller_spec.rb
+++ spec/controllers/compositions_controller_spec.rb
@@ -29,5 +29,92 @@ RSpec.describe CompositionsController do
       }
       expect(response).to be_success, response.body
     end
+
+    it 'creates a new composition for authenticated user' do
+      sign_in @user
+
+      expect do
+        post :save, params: {
+          player_name: 'chocotaco', hero_id: @hero1.id,
+          map_segment_id: @map_segment.id, format: :json
+        }
+      end.to change { Composition.count }.by(1)
+    end
+
+    it 'creates a new player for new name for authenticated user' do
+      sign_in @user
+
+      expect do
+        post :save, params: {
+          player_name: 'chocotaco', hero_id: @hero1.id,
+          map_segment_id: @map_segment.id, format: :json
+        }
+      end.to change { Player.count }.by(1)
+    end
+
+    it 'reuses existing player for authenticated user' do
+      sign_in @user
+      player = create(:player, creator: @user)
+
+      expect do
+        post :save, params: {
+          player_name: player.name, hero_id: @hero1.id,
+          map_segment_id: @map_segment.id, format: :json
+        }
+      end.not_to change { Player.count }
+    end
+
+    it 'creates a new player-hero for new combination for auth user' do
+      sign_in @user
+
+      expect do
+        post :save, params: {
+          player_name: 'chocotaco', hero_id: @hero1.id,
+          map_segment_id: @map_segment.id, format: :json
+        }
+      end.to change { PlayerHero.count }.by(1)
+    end
+
+    it 'reuses an existing player-hero combination for auth user' do
+      sign_in @user
+      player = create(:player, creator: @user)
+      player_hero = create(:player_hero, player: player, hero: @hero1)
+
+      expect do
+        post :save, params: {
+          player_name: player.name, hero_id: @hero1.id,
+          map_segment_id: @map_segment.id, format: :json
+        }
+      end.not_to change { PlayerHero.count }
+    end
+
+    it 'creates a new player selection for authenticated user' do
+      sign_in @user
+
+      expect do
+        post :save, params: {
+          player_name: 'chocotaco', hero_id: @hero1.id,
+          map_segment_id: @map_segment.id, format: :json
+        }
+      end.to change { PlayerSelection.count }.by(1)
+    end
+
+    it 'no-op for existing player selection for authenticated user' do
+      sign_in @user
+
+      player = create(:player, creator: @user)
+      player_hero = create(:player_hero, player: player, hero: @hero1)
+      composition = create(:composition, user: @user, map: @map)
+      player_selection = create(:player_selection, composition: composition,
+                                player_hero: player_hero,
+                                map_segment: @map_segment)
+
+      expect do
+        post :save, params: {
+          player_name: player.name, hero_id: @hero1.id, format: :json,
+          composition_id: composition.id, map_segment_id: @map_segment.id
+        }
+      end.not_to change { PlayerSelection.count }
+    end
   end
 end
