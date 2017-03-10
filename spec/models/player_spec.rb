f8c81c986774ec536069b44d9c1d839a2d7be4fd
Sarah Vessels <cheshire137@gmail.com>
2017-03-10 17:18:13 -0600

    Add new Player validations for creator

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -6,4 +6,17 @@ describe Player do
     expect(player.valid?).to be_falsey
     expect(player.errors[:name].any?).to be_truthy
   end
+
+  it 'requires a creator' do
+    player = Player.new
+    expect(player.valid?).to be_falsey
+    expect(player.errors[:creator].any?).to be_truthy
+  end
+
+  it 'requires a creator session ID if the creator is anonymous' do
+    anon_user = create(:anonymous_user)
+    player = Player.new(creator: anon_user)
+    expect(player.valid?).to be_falsey
+    expect(player.errors[:creator_session_id].any?).to be_truthy
+  end
 end
