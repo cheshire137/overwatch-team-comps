41d23f92b0937e746988dfb36d2c148f6d86876c
Sarah Vessels <cheshire137@gmail.com>
2017-03-10 17:38:21 -0600

    Validate session_id set on comp if user is anon

--- app/models/composition.rb
+++ app/models/composition.rb
@@ -11,14 +11,29 @@ class Composition < ApplicationRecord
 
   before_validation :set_name
 
-  validates :map, :session_id, :name, presence: true
+  validates :map, :user, :name, presence: true
   validates :name, uniqueness: { scope: [:map_id, :user_id] }
+  validate :session_id_set_if_anonymous
 
   def set_name
     return unless user
     return if name.present?
 
-    num_comps = user.compositions.count
+    num_comps = if user.anonymous?
+      Composition.where(session_id: session_id).count
+    else
+      user.compositions.count
+    end
+
     self.name = "Composition #{num_comps + 1}"
+  end
+
+  private
+
+  def session_id_set_if_anonymous
+    return unless user && user.anonymous?
+    return if session_id.present?
+
+    errors.add(:session_id, 'is required if user is anonymous.')
   end
 end
--- spec/models/composition_spec.rb
+++ spec/models/composition_spec.rb
@@ -7,6 +7,19 @@ describe Composition do
     expect(composition.errors[:map].any?).to be_truthy
   end
 
+  it 'requires a user' do
+    composition = Composition.new
+    expect(composition.valid?).to be_falsey
+    expect(composition.errors[:user].any?).to be_truthy
+  end
+
+  it 'requires a session ID if the user is anonymous' do
+    anon_user = create(:anonymous_user)
+    composition = Composition.new(user: anon_user)
+    expect(composition.valid?).to be_falsey
+    expect(composition.errors[:session_id].any?).to be_truthy
+  end
+
   it 'sets composition name before validation' do
     user = create(:user)
     composition = Composition.new(user: user)
