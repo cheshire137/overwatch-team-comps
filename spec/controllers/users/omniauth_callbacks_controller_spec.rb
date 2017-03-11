7b0021b4283247282dbebc54549bc0f04ca2bad9
Sarah Vessels <cheshire137@gmail.com>
2017-03-11 13:16:53 -0600

    Test finish_signup page

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Users::OmniauthCallbacksController do
+  render_views
+
   before(:each) do
     OmniAuth.config.test_mode = true
 
     OmniAuth.config.mock_auth[:bnet] = OmniAuth::AuthHash.new(
       provider: 'bnet',
-      uid: '123456'
+      uid: '123456',
+      info: { battletag: 'coollady#1965' }
     )
 
     @request.env['devise.mapping'] = Devise.mappings[:user]
@@ -16,7 +19,7 @@ RSpec.describe Users::OmniauthCallbacksController do
   describe 'POST bnet' do
     it 'continues sign-up flow for new user' do
       expect { post :bnet }.not_to change { User.count }
-      expect(session['devise.bnet_data']).not_to be_nil
+      expect(session['devise.bnet_data']).to eq(OmniAuth.config.mock_auth[:bnet])
       expect(response).to redirect_to('/users/finish_signup')
     end
 
@@ -25,6 +28,14 @@ RSpec.describe Users::OmniauthCallbacksController do
       expect { post :bnet }.not_to change { User.count }
       expect(session['devise.bnet_data']).to be_nil
       expect(response).to redirect_to('/')
+    end
+  end
+
+  describe 'GET finish_signup' do
+    it 'loads successfully' do
+      get :finish_signup, session: { 'devise.bnet_data' => OmniAuth.config.mock_auth[:bnet] }
+      expect(response).to be_success
+      expect(response.body).to include('coollady#1965')
     end
   end
 end
