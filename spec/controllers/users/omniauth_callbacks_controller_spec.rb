28fb43a5719e8537c53ad1dd126fade7e1110f44
Sarah Vessels <cheshire137@gmail.com>
2017-03-11 13:27:44 -0600

    Flesh out more tests for Omniauth endpoints

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Users::OmniauthCallbacksController do
-  render_views
-
   before(:each) do
     OmniAuth.config.test_mode = true
 
@@ -32,10 +30,41 @@ RSpec.describe Users::OmniauthCallbacksController do
   end
 
   describe 'GET finish_signup' do
+    render_views
+
     it 'loads successfully' do
       get :finish_signup, session: { 'devise.bnet_data' => OmniAuth.config.mock_auth[:bnet] }
       expect(response).to be_success
       expect(response.body).to include('coollady#1965')
+    end
+  end
+
+  describe 'POST finished_signup' do
+    render_views
+
+    it 'creates a user' do
+      expect do
+        post :finished_signup, params: { email: 'test@example.com' },
+          session: { 'devise.bnet_data' => OmniAuth.config.mock_auth[:bnet] }
+      end.to change { User.count }.by(1)
+      expect(response).to redirect_to('/')
+      expect(flash[:notice]).to eq('Successfully authenticated from Battle.net account.')
+
+      user = User.last
+      expect(user.email).to eq('test@example.com')
+      expect(user.provider).to eq('bnet')
+      expect(user.uid).to eq('123456')
+      expect(user.battletag).to eq('coollady#1965')
+    end
+
+    it 'does not save user when email is omitted' do
+      expect do
+        post :finished_signup, params: { email: '' },
+          session: { 'devise.bnet_data' => OmniAuth.config.mock_auth[:bnet] }
+      end.not_to change { User.count }
+      expect(response).to be_success
+      expect(response.body).to include('coollady#1965')
+      expect(response.body).to include('Please provide an email address.')
     end
   end
 end
