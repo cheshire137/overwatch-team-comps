189e62b22796581ac5c293ea494462a370129249
Sarah Vessels <cheshire137@gmail.com>
2017-03-10 19:13:28 -0600

    Add Player#get_name helper

--- app/models/player.rb
+++ app/models/player.rb
@@ -7,6 +7,20 @@ class Player < ApplicationRecord
   validates :name, :creator, presence: true
   validate :creator_session_id_set_if_anonymous
 
+  # Given a list of names for players already in a composition, this will
+  # return a reasonable default name for a new player.
+  def self.get_name(existing_names)
+    existing_default_names = existing_names.uniq.sort.select do |name|
+      name =~ /Player \d/
+    end
+    existing_numbers = existing_default_names.map do |name|
+      name.split('Player ')[1].to_i
+    end
+    default_numbers = [1, 2, 3, 4, 5, 6]
+    next_number = (default_numbers - existing_numbers).first
+    "Player #{next_number}"
+  end
+
   private
 
   def creator_session_id_set_if_anonymous
