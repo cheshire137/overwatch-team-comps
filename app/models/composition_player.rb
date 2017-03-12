fe902fd75a77901f3f546da76cdf065d303b5ada
Sarah Vessels <cheshire137@gmail.com>
2017-03-12 13:42:52 -0500

    Ensure player owner is same as comp owner

--- app/models/composition_player.rb
+++ app/models/composition_player.rb
@@ -11,6 +11,7 @@ class CompositionPlayer < ApplicationRecord
     inclusion: 0...Composition::MAX_PLAYERS
   validates :player_id, uniqueness: { scope: :composition_id }
   validate :composition_does_not_have_max_players
+  validate :player_is_allowed_for_comp_owner
 
   default_scope { order(:position) }
 
@@ -30,6 +31,20 @@ class CompositionPlayer < ApplicationRecord
 
     if scope.count > Composition::MAX_PLAYERS - 1
       errors.add(:composition, 'has maximum number of players already.')
+    end
+  end
+
+  def player_is_allowed_for_comp_owner
+    return unless player && composition
+
+    if player.creator.anonymous?
+      unless composition.user.anonymous? && composition.session_id == creator_session_id
+        errors.add(:player, 'is not valid.')
+      end
+    else
+      unless composition.user == player.creator
+        errors.add(:player, 'is not valid.')
+      end
     end
   end
 end
