ec8a47c79d3df11fe16814e34d407f045b914b13
Sarah Vessels <cheshire137@gmail.com>
2017-03-11 18:26:13 -0600

    Move player, comp updates to model

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -20,10 +20,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
                     password: Devise.friendly_token[0,20])
 
     if user.save
-      Player.where(creator_id: User.anonymous, creator_session_id: session.id).
-             update_all(creator_id: user.id)
-      Composition.where(user_id: User.anonymous, session_id: session.id).
-                  update_all(user_id: user.id)
+      user.migrate_session_records(session.id)
 
       session['devise.bnet_data'] = nil
       set_flash_message(:notice, :success, kind: "Battle.net")
