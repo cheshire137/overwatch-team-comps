b655f87b2fc24a7b5d097a0bb5f786bba15b7e35
Sarah Vessels <cheshire137@gmail.com>
2017-03-12 17:27:50 -0500

    Add order_by_name, find_if_allowed to Player

--- app/models/player.rb
+++ app/models/player.rb
@@ -8,6 +8,23 @@ class Player < ApplicationRecord
   validate :creator_session_id_set_if_anonymous
   validate :name_is_unique_to_creator
 
+  scope :order_by_name, ->{ order('UPPER(name) ASC') }
+
+  # Returns the Player with the given ID, but only if that Player is
+  # owned by the given User/session.
+  #
+  # Returns Player or nil.
+  def self.find_if_allowed(id, user:, session_id:)
+    scope = if user
+      where('id = ? AND creator_id = ?', id, user)
+    else
+      where('id = ? AND creator_id = ? AND creator_session_id = ?',
+            id, User.anonymous, session_id)
+    end
+
+    scope.first
+  end
+
   # Given a list of names for players already in a composition, this will
   # return a reasonable default name for a new player.
   def self.get_name(existing_names)
