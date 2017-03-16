4963e37de2f466255d6c0061be3f061f2c912900
Sarah Vessels <cheshire137@gmail.com>
2017-03-15 21:19:40 -0500

    Fix style linter error about pure stateless function

--- app/assets/javascripts/components/edit-player-selection-row.jsx
+++ app/assets/javascripts/components/edit-player-selection-row.jsx
@@ -1,35 +1,33 @@
 import HeroSelect from './hero-select.jsx'
 import PlayerSelect from './player-select.jsx'
 
-class EditPlayerSelectionRow extends React.Component {
-  render() {
-    const { inputID, selectedPlayer, nameLabel, onHeroSelection,
-            mapSegments, onPlayerSelection, players, heroes,
-            selections } = this.props
-    return (
-      <tr>
-        <td className="player-cell">
-          <PlayerSelect
-            inputID={inputID}
-            label={nameLabel}
-            selectedPlayer={selectedPlayer}
-            players={players}
-            onChange={(playerID, name) => onPlayerSelection(playerID, name)}
+const EditPlayerSelectionRow = function(props) {
+  const { inputID, selectedPlayer, nameLabel, onHeroSelection,
+          mapSegments, onPlayerSelection, players, heroes,
+          selections } = props
+  return (
+    <tr>
+      <td className="player-cell">
+        <PlayerSelect
+          inputID={inputID}
+          label={nameLabel}
+          selectedPlayer={selectedPlayer}
+          players={players}
+          onChange={(playerID, name) => onPlayerSelection(playerID, name)}
+        />
+      </td>
+      {mapSegments.map(segment => (
+        <td key={segment.id} className="hero-select-cell">
+          <HeroSelect
+            heroes={heroes}
+            disabled={typeof selectedPlayer.id !== 'number'}
+            selectedHeroID={selections[segment.id]}
+            onChange={heroID => onHeroSelection(heroID, segment.id)}
           />
         </td>
-        {mapSegments.map(segment => (
-          <td key={segment.id} className="hero-select-cell">
-            <HeroSelect
-              heroes={heroes}
-              disabled={typeof selectedPlayer.id !== 'number'}
-              selectedHeroID={selections[segment.id]}
-              onChange={heroID => onHeroSelection(heroID, segment.id)}
-            />
-          </td>
-        ))}
-      </tr>
-    )
-  }
+      ))}
+    </tr>
+  )
 }
 
 EditPlayerSelectionRow.propTypes = {
