3c027d86a0095a4e45ba412968fc180e371f9a89
Sarah Vessels <cheshire137@gmail.com>
2017-03-16 18:45:03 -0500

    List radio buttons for each hero in pool

--- app/assets/javascripts/components/hero-pool-choice.jsx
+++ app/assets/javascripts/components/hero-pool-choice.jsx
@@ -1,10 +1,40 @@
+const confidenceRanks = {
+  bronze: 0,
+  silver: 15,
+  gold: 30,
+  platinum: 45,
+  diamond: 60,
+  master: 75,
+  grandmaster: 100
+}
+
 class HeroPoolChoice extends React.Component {
+  onChange(event) {
+    console.log(this.props.hero.name, event.target.value)
+  }
+
   render() {
     const { hero } = this.props
+    const ranks = Object.keys(confidenceRanks)
 
     return (
       <div key={hero.id}>
         {hero.name}
+        <div>
+          {ranks.map(rank => {
+            return (
+              <div key={rank}>
+                <input
+                  name={hero.name}
+                  type="radio"
+                  checked={hero.confidence === confidenceRanks[rank]}
+                  value={confidenceRanks[rank]}
+                  onChange={e => this.onChange(e)}
+                />
+              </div>
+            )
+          })}
+        </div>
       </div>
     )
   }
