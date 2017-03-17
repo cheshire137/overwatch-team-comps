02dd70aa4557f0221a33b459b0ff0fb8ac9b0030
Sarah Vessels <cheshire137@gmail.com>
2017-03-17 18:15:41 -0500

    Add HeroesController tests

---
+++ spec/requests/heroes_apis_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'heroes API', type: :request do
+  before(:each) do
+    @user = create(:user)
+    @hero1 = create(:hero, name: 'Ana')
+    @hero2 = create(:hero, name: 'Pharah')
+  end
+
+  describe "GET pool" do
+    it 'includes all heroes for authenticated user' do
+      sign_in @user
+
+      get '/api/heroes/pool'
+
+      expect(response.body).to include(@hero1.name)
+      expect(response.body).to include(@hero2.name)
+    end
+  end
+end
