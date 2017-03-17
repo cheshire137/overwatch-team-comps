ee23b1ea133f36f40faf6cf4d2f48bf01705c33a
Sarah Vessels <cheshire137@gmail.com>
2017-03-17 18:06:50 -0500

    Render JSON Devise errors for JSON request

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,6 +259,9 @@ Devise.setup do |config|
   #   manager.intercept_401 = false
   #   manager.default_strategies(scope: :user).unshift :some_external_strategy
   # end
+  config.warden do |manager|
+    manager.failure_app = OverwatchTeamCompsDeviseFailureApp
+  end
 
   # ==> Mountable engine configurations
   # When using Devise inside an engine, let's call it `MyEngine`, and this engine
