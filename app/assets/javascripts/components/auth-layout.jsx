f405a54b8d28bafabcf46e263a7471257ff7d649
Sarah Vessels <cheshire137@gmail.com>
2017-03-18 17:46:08 -0500

    Finish AuthLayout

--- app/assets/javascripts/components/auth-layout.jsx
+++ app/assets/javascripts/components/auth-layout.jsx
@@ -1,5 +1,15 @@
+import LocalStorage from '../models/local-storage'
+
 class AuthLayout extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      battletag: LocalStorage.get('battletag')
+    }
+  }
+
   render() {
+    const path = this.props.location.pathname
     return (
       <div>
         <div className="container">
@@ -7,13 +17,15 @@ class AuthLayout extends React.Component {
             <div className="nav-right">
               <a
                 href="/user"
-                className="nav-item"
+                className={`nav-item ${path === '/user' ? 'is-active' : ''}`}
               >Team composition</a>
               <a
                 href="/user/hero-pool"
-                className="nav-item"
+                className={`nav-item ${path === '/user/hero-pool' ? 'is-active' : ''}`}
               >Your hero pool</a>
-              <span className="nav-item">Signed in as <strong></strong></span>
+              <span className="nav-item">
+                Signed in as <strong>{this.state.battletag}</strong>
+              </span>
             </div>
           </nav>
         </div>
@@ -26,7 +38,8 @@ class AuthLayout extends React.Component {
 }
 
 AuthLayout.propTypes = {
-  children: React.PropTypes.object.isRequired
+  children: React.PropTypes.object.isRequired,
+  location: React.PropTypes.object.isRequired
 }
 
 export default AuthLayout
