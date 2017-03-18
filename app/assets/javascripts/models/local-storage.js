563fab3aa483b8c9e3041fe053ad6f1cc485324c
Sarah Vessels <cheshire137@gmail.com>
2017-03-18 17:39:01 -0500

    Add LocalStorage JS model

---
+++ app/assets/javascripts/models/local-storage.js
@@ -0,0 +1,35 @@
+const localStorageKey = 'overwatch-team-comps'
+
+export default class LocalStorage {
+  static getJSON() {
+    if (!window.localStorage) {
+      console.error('browser does not support local storage')
+      return {}
+    }
+
+    const appData = window.localStorage.getItem(localStorageKey) || '{}'
+    return JSON.parse(appData)
+  }
+
+  static has(key) {
+    const value = this.get(key)
+    return typeof value !== 'undefined'
+  }
+
+  static get(key) {
+    const appData = this.getJSON()
+    return appData[key]
+  }
+
+  static set(key, value) {
+    const appData = this.getJSON()
+    appData[key] = value
+    window.localStorage.setItem(localStorageKey, JSON.stringify(appData))
+  }
+
+  static delete(key) {
+    const appData = this.getJSON()
+    delete appData[key]
+    window.localStorage.setItem(localStorageKey, JSON.stringify(appData))
+  }
+}
