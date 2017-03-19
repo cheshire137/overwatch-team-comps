51e00d2ab9b7eeb4168ba8d21c30ff2cdc919732
Sarah Vessels <cheshire137@gmail.com>
2017-03-18 19:13:55 -0500

    Show hero portrait in comp form

--- app/assets/javascripts/components/hero-select.jsx
+++ app/assets/javascripts/components/hero-select.jsx
@@ -4,24 +4,42 @@ class HeroSelect extends React.Component {
     this.props.onChange(heroID)
   }
 
+  heroPortrait() {
+    const { heroes, selectedHeroID } = this.props
+    if (typeof selectedHeroID !== 'number') {
+      return <span className="hero-portrait-placeholder" />
+    }
+    const hero = heroes.filter(h => h.id === selectedHeroID)[0]
+    return (
+      <img
+        src={hero.image}
+        alt={hero.name}
+        className="hero-portrait"
+      />
+    )
+  }
+
   render() {
     const { heroes, selectedHeroID, disabled } = this.props
     return (
-      <span className="select">
-        <select
-          onChange={e => this.onChange(e)}
-          value={selectedHeroID || ''}
-          disabled={disabled}
-        >
-          <option>Choose a hero</option>
-          {heroes.map(hero => (
-            <option
-              key={hero.id}
-              value={hero.id}
-            >{hero.name}</option>
-          ))}
-        </select>
-      </span>
+      <div>
+        {this.heroPortrait()}
+        <span className="select">
+          <select
+            onChange={e => this.onChange(e)}
+            value={selectedHeroID || ''}
+            disabled={disabled}
+          >
+            <option>Choose a hero</option>
+            {heroes.map(hero => (
+              <option
+                key={hero.id}
+                value={hero.id}
+              >{hero.name}</option>
+            ))}
+          </select>
+        </span>
+      </div>
     )
   }
 }
