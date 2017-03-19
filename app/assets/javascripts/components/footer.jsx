dcd53ba1e0021ca79371be000250c3065e60d508
Sarah Vessels <cheshire137@gmail.com>
2017-03-19 16:04:43 -0500

    Add footer link to view source code

--- app/assets/javascripts/components/footer.jsx
+++ app/assets/javascripts/components/footer.jsx
@@ -4,6 +4,11 @@ const Footer = function(props) {
     <footer className="footer">
       <div className="container">
         <a href={`${basePath}/about`}>About</a>
+        <a
+          href="https://github.com/cheshire137/overwatch-team-comps"
+          target="_blank"
+          rel="noopener noreferrer"
+        >View source</a>
       </div>
     </footer>
   )
