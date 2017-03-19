27e765aa205ea565cf928f4c226832c6f943e4e3
Sarah Vessels <cheshire137@gmail.com>
2017-03-19 15:56:28 -0500

    Mention Blizzard, React, Rails on About page

--- app/assets/javascripts/components/about.jsx
+++ app/assets/javascripts/components/about.jsx
@@ -1,9 +1,20 @@
-const About = function(props) {
+const About = function() {
   return (
-    <div className="container">
+    <div className="container about-page">
       <h1>About</h1>
       <p>
-        Placeholder for Rob to figure out. &lt;3
+        Thanks to Blizzard for Overwatch. <i
+          className="fa fa-heart"
+          aria-hidden="true"
+        /> This app was built with <a
+          href="https://facebook.github.io/react/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >React</a> and <a
+          href="http://rubyonrails.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >Ruby on Rails</a>.
       </p>
     </div>
   )
