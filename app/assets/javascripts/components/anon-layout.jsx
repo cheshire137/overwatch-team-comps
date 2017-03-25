b8c73b1de80d7753b6e4f76de77d05d8925ba979
Sarah Vessels <cheshire137@gmail.com>
2017-03-25 10:10:25 -0500

    Link back to team comp from About page when signed out

--- app/assets/javascripts/components/anon-layout.jsx
+++ app/assets/javascripts/components/anon-layout.jsx
@@ -1,11 +1,18 @@
 import Footer from './footer.jsx'
 
 const AnonLayout = function(props) {
+  const path = props.location.pathname
   return (
     <div className="layout-container">
       <div className="container">
         <nav className="nav">
           <div className="nav-right">
+            {path === '/about' ? (
+              <a
+                href="/"
+                className="nav-item"
+              >Team composition</a>
+            ) : ''}
             <a
               href="/users/auth/bnet"
               className="nav-item"
@@ -22,7 +29,8 @@ const AnonLayout = function(props) {
 }
 
 AnonLayout.propTypes = {
-  children: React.PropTypes.object.isRequired
+  children: React.PropTypes.object.isRequired,
+  location: React.PropTypes.object.isRequired
 }
 
 export default AnonLayout
