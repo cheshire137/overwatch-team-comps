a8724d0b4d6eed797011b77f16096535d0b87bf8
Sarah Vessels <cheshire137@gmail.com>
2017-04-09 13:29:20 -0500

    Fix local storage warnings from settings test

--- spec/javascript/components/settings.test.jsx
+++ spec/javascript/components/settings.test.jsx
@@ -1,11 +1,23 @@
 import renderer from 'react-test-renderer'
 
+import mockLocalStorage from '../mocks/local-storage'
+
 import Settings from '../../../app/assets/javascripts/components/settings.jsx'
 
 describe('Settings', () => {
   let component = null
+  let store = null
 
   beforeEach(() => {
+    store = {
+      'overwatch-team-comps': JSON.stringify({
+        email: 'user@example.com',
+        region: 'eu',
+        platform: 'xbl'
+      })
+    }
+    mockLocalStorage(store)
+
     component = <Settings />
   })
 
