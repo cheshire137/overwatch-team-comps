9f125431057514218cfa31ca215b48427918c249
Sarah Vessels <cheshire137@gmail.com>
2017-07-16 17:27:25 -0500

    Make use of SelectMenu in MapSelect

--- app/assets/javascripts/components/map-select.jsx
+++ app/assets/javascripts/components/map-select.jsx
@@ -1,91 +1,34 @@
 import PropTypes from 'prop-types'
-import onClickOutside from 'react-onclickoutside'
 
-class MapSelect extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { isOpen: false }
-  }
+import SelectMenu from './select-menu.jsx'
 
-  onMapChange(event) {
-    this.props.onChange(event.target.value)
-  }
+const MapSelect = function(props) {
+  const { selectedMapID, disabled, maps, onChange } = props
+  const selectedMap = maps.filter(m => m.id === selectedMapID)[0]
 
-  onMenuItemClick(event, mapID) {
-    event.preventDefault()
-    event.target.blur()
-    if (this.props.disabled) {
-      return
-    }
-    this.setState({ isOpen: false }, () => {
-      this.props.onChange(mapID)
-    })
-  }
-
-  containerClass() {
-    const classes = ['map-select-container menu-container']
-    if (this.state.isOpen) {
-      classes.push('open')
-    }
-    return classes.join(' ')
-  }
-
-  handleClickOutside() {
-    if (this.state.isOpen) {
-      this.setState({ isOpen: false })
-    }
-  }
-
-  menuClass() {
-    const classes = ['menu']
-    if (this.props.disabled) {
-      classes.push('is-disabled')
-    }
-    return classes.join(' ')
-  }
-
-  toggleMenuOpen(event) {
-    event.target.blur()
-    this.setState({ isOpen: !this.state.isOpen })
-  }
-
-  render() {
-    const { selectedMapID, disabled, maps } = this.props
-    const selectedMapName = maps.filter(m => m.id === selectedMapID)[0].name
-
-    return (
-      <div className={this.containerClass()}>
-        <button
-          type="button"
-          disabled={disabled}
-          className={`button menu-toggle ${disabled ? 'is-disabled' : ''}`}
-          onClick={e => this.toggleMenuOpen(e)}
-        >
+  return (
+    <SelectMenu
+      items={maps}
+      disabled={disabled}
+      selectedItemID={selectedMapID}
+      onChange={val => onChange(val)}
+      containerClass={() => 'map-select-container'}
+      menuToggleContents={() => (
+        <span>
           <i
             className="fa fa-map-marker"
             aria-hidden="true"
-          /> {selectedMapName} <i className="fa fa-caret-down" aria-hidden="true" />
-        </button>
-        <div className={this.menuClass()}>
-          {maps.map(map => {
-            const isSelected = map.id === selectedMapID
-            return (
-              <button
-                key={map.id}
-                className={`map-${map.slug} menu-item button ${isSelected ? 'is-selected' : ''}`}
-                onClick={e => this.onMenuItemClick(e, map.id)}
-              >
-                {map.name}
-                {isSelected ? (
-                  <i aria-hidden="true" className="fa fa-check menu-item-selected-indicator" />
-                ) : ''}
-              </button>
-            )
-          })}
-        </div>
-      </div>
-    )
-  }
+          /> {selectedMap.name}
+        </span>
+      )}
+      menuItemClass={map => `map-${map.slug}`}
+      menuItemContent={(map, isSelected) => (
+        <span className={isSelected ? 'with-selected' : ''}>
+          {map.name}
+        </span>
+      )}
+    />
+  )
 }
 
 MapSelect.propTypes = {
@@ -95,4 +38,4 @@ MapSelect.propTypes = {
   onChange: PropTypes.func.isRequired
 }
 
-export default process.env.NODE_ENV === 'test' ? MapSelect : onClickOutside(MapSelect)
+export default MapSelect
