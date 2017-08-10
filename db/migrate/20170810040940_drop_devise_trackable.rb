ab08a9c10510fec32b632bdc5e6850c8599e9d30
Sarah Vessels <cheshire137@gmail.com>
2017-08-09 23:12:08 -0500

    Drop :trackable Devise module from User

---
+++ db/migrate/20170810040940_drop_devise_trackable.rb
@@ -0,0 +1,17 @@
+class DropDeviseTrackable < ActiveRecord::Migration[5.0]
+  def up
+    remove_column :users, :sign_in_count
+    remove_column :users, :current_sign_in_at
+    remove_column :users, :last_sign_in_at
+    remove_column :users, :current_sign_in_ip
+    remove_column :users, :last_sign_in_ip
+  end
+
+  def down
+    add_column :users, :sign_in_count, :integer, default: 0, null: false
+    add_column :users, :current_sign_in_at, :datetime
+    add_column :users, :last_sign_in_at, :datetime
+    add_column :users, :current_sign_in_ip, :string
+    add_column :users, :last_sign_in_ip, :string
+  end
+end
